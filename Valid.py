060816b4e9ebe66edf0042137e1e851f0e9956c4
Bill Gots <102953387+Bill-Gots@users.noreply.github.com>
2022-08-24 21:42:09 +0800

    update valid

--- Valid.py
+++ Valid.py
@@ -2,20 +2,19 @@ import torch
 from torch.utils.data import DataLoader
 
 
-def valid(model, v_dataset, top_k, arg):
+def valid(model, v_dataset, arg, device):
     data_loaders = DataLoader(v_dataset, batch_size = arg.batchsize, shuffle = True)
     acc_number = 0
     for id, batch in enumerate(data_loaders):
         images, labels = batch
-        images, labels = images.cuda(), labels.cuda()
+        images, labels = images.to(device), labels.to(device)
         outputs = model(images)
-        if categories:= outputs.shape[1] <= top_k:
+        if categories:= outputs.shape[1] <= arg.top_k:
             print('It\'s meaningless to compute top{0:top_k} accuracy on a dataset with {1:categories} \
-                categories.'.format(top_k = top_k, categories = categories))
+                categories.'.format(top_k = arg.top_k, categories = categories))
             return 1.0
         else:
-            #outputs = outputs.copy()
-            for i in range(0, top_k):
+            for i in range(0, arg.top_k):
                 if outputs.shape[0]:
                     idxmax = outputs.argmax(dim = 1)
                     idxeq = idxmax.eq(labels)
