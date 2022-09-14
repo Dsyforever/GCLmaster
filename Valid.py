41ddc5d276ee2518c119846103059d658baa6fe7
Bill Gots <102953387+Bill-Gots@users.noreply.github.com>
2022-09-14 23:15:50 +0800

    update Valid

--- Valid.py
+++ Valid.py
@@ -2,33 +2,27 @@ import torch
 from torch.utils.data import DataLoader
 
 
-def valid(model, v_dataset, arg, device):
+def valid(model, v_dataset, arg):
     data_loaders = DataLoader(v_dataset, batch_size = arg.batchsize, shuffle = True)
     acc_number = 0
-    for id, batch in enumerate(data_loaders):
-        images, labels = batch
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        if categories:= outputs.shape[1] <= arg.top_k:
-            print('It\'s meaningless to compute {0:top_k} accuracy on a dataset with {1:categories} \
-                categories.'.format(top_k = arg.top_k, categories = categories))
-        images, labels = images.cuda(), labels.cuda()
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
+    with torch.no_grad():
         if categories:= outputs.shape[1] <= arg.top_k:
             print('It\'s meaningless to compute top{0:top_k} accuracy on a dataset with {1:categories} \
                 categories.'.format(top_k = arg.top_k, categories = categories))
             return 1.0
-        else:
-            #outputs = outputs.copy()
-            for i in range(0, arg.top_k):
-                if outputs.shape[0]:
-                    idxmax = outputs.argmax(dim = 1)
-                    idxeq = idxmax.eq(labels)
-                    acc_number += idxeq.sum().item()
-                    outputs[torch.arange(0, outputs.shape[0], 1), idxmax] = -1
-                    idxeq = (idxeq == False)
-                    outputs, labels = outputs[idxeq], labels[idxeq]
-            accuracy = acc_number / len(v_dataset)
-    return accuracy
+        else: 
+            for _, batch in enumerate(data_loaders):
+                images, labels = batch
+                images, labels = images.cuda(), labels.cuda()
+                outputs = model(images)
+
+                for __ in range(0, arg.top_k):
+                    if outputs.shape[0]:
+                        idxmax = outputs.argmax(dim = 1)
+                        idxeq = idxmax.eq(labels)
+                        acc_number += idxeq.sum().item()
+                        outputs[torch.arange(0, outputs.shape[0], 1), idxmax] = -1
+                        idxeq = (idxeq == False)
+                        outputs, labels = outputs[idxeq], labels[idxeq]
+                accuracy = acc_number / len(v_dataset)
+            return accuracy
