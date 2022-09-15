5d1eb2c201926af635cc7fea6da4064df4e6bcca
Bill Gots <102953387+Bill-Gots@users.noreply.github.com>
2022-09-15 10:59:10 +0800

    update valid

--- Valid.py
+++ Valid.py
@@ -6,16 +6,15 @@ def valid(model, v_dataset, arg):
     data_loaders = DataLoader(v_dataset, batch_size = arg.batchsize, shuffle = True)
     acc_number = 0
     with torch.no_grad():
-        if categories:= outputs.shape[1] <= arg.top_k:
-            print('It\'s meaningless to compute top{0:top_k} accuracy on a dataset with {1:categories} \
-                categories.'.format(top_k = arg.top_k, categories = categories))
-            return 1.0
-        else: 
-            for _, batch in enumerate(data_loaders):
-                images, labels = batch
-                images, labels = images.cuda(), labels.cuda()
-                outputs = model(images)
-
+        for _, batch in enumerate(data_loaders):
+            images, labels = batch
+            images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)
+            if categories:= outputs.shape[1] <= arg.top_k:
+                print('It\'s meaningless to compute top{0:top_k} accuracy on a dataset with {1:categories} \
+                    categories.'.format(top_k = arg.top_k, categories = categories))
+                return 1.0
+            else: 
                 for __ in range(0, arg.top_k):
                     if outputs.shape[0]:
                         idxmax = outputs.argmax(dim = 1)
